6b3ee8259a6444ccacece8fcf76e0ac70174b928
natashabuck <natashabuck1@gmail.com>
2018-11-26 15:37:17 +0000

    Done.

--- src/taskList.js
+++ src/taskList.js
@@ -1,2 +1,45 @@
-class TaskList {
+const formEl = document.querySelector('#create-task-form');
+const descInput = document.querySelector('#new-task-description');
+const taskList = document.querySelector('#tasks');
+
+const addTask = task => {
+    const taskItem = document.createElement('li')
+    
+    taskItem.className = 'task-item'
+    taskItem.id = task.id
+
+    taskItem.innerHTML = `
+    ${task.description}`
+    
+    taskList.appendChild(taskItem)
 }
+    
+const addTasks = tasks => {
+    tasks.forEach(task => addTask(task))
+}
+
+formEl.addEventListener('submit', (event) => {
+        event.preventDefault()
+        task = {
+            description: descInput.value
+        }
+        createTask(task)
+            .then(serverTask => addTask(serverTask))
+
+        formEl.reset()
+    })
+
+const getTasks = () =>
+    fetch('http://localhost:3000/tasks')
+        .then(resp => resp.json())
+
+const createTask = task => 
+    fetch('http://localhost:3000/tasks', {
+        method: 'POST', 
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(task)
+    })
+    .then(resp => resp.json())
+
+getTasks()
+    .then(tasks=> addTasks(tasks))
